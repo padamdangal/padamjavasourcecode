b0488900463323a8257f066b58a503cbcc5c01e5
padmadangal1@gmail.com <padmadangal1@gmail.com@d3babca3-09bd-c287-c0c1-2540b1b037a8>
2015-03-15 03:39:59 +0000

    Initial import by Padam Dangal

---
+++ trunk/SessionExampleInServlet/src/LoginServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String fname = request.getParameter("firstname");
+		String lname = request.getParameter("lastname");
+		
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("name", fname + " " + lname);
+		
+		request.getRequestDispatcher("home.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
